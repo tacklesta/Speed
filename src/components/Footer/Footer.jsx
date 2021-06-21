3f9089c9fb5804936fe7ea16cc8f5f64459ddea3
Tarun <tarundyundi2000@gmail.com>
2021-06-21 19:49:57 +0530

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,19 +2,16 @@ import React from 'react';
 import './Footer.css';
 
 const Footer = () => {
-    return ( <
-        div >
-        <
-        div className = 'footer-container' >
-        <
-        a href = "xyz.com"
+    return ( 
+        <div className ='footer-container'>
+        <a 
+        href = "xyz.com"
         target = "_blank"
         className = "footer-link"
         rel = "noreferrer" >
-        Welcome to World of Tacklestar < /a> < /
-        div >
-        <
-        /div>
+        Welcome to World of Tacklestar 
+        </a> 
+        </div>
     );
 };
 
