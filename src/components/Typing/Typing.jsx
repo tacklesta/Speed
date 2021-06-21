9032271037c8a6a316c067f001b420212fce10fc
Tarun <tarundyundi2000@gmail.com>
2021-06-21 20:05:33 +0530

    Update Typing.jsx

--- src/components/Typing/Typing.jsx
+++ src/components/Typing/Typing.jsx
@@ -8,45 +8,40 @@ const Typing = ({
     testInfo,
     oninputchange
 }) => {
-    return ( <
-        div className = "typing-container" >
-        <
-        div class = "timer-container" >
-        <
-        p className = "timer" >
-        00: { timeRemaining >= 10 ? timeRemaining : '0${timeRemaining}' } <
-        /p> <
-        p className = "timer-info" > {!timerstarted && "ARE YOU READY !!" } <
-        /p> < /
-        div >
+    return ( 
+        <div className = "typing-container">
+        <div class = "timer-container">
+        <p className = "timer">
+                00: 
+                    {timeRemaining >= 10 ? timeRemaining : '0${timeRemaining}'} 
+        </p> 
+        <p className="timer-info"> 
+            {!timerstarted && "ARE YOU READY !!" } 
+        </p> 
+        </div>
 
-        <
-        div className = "text-area-container" >
-        <
-        div className = "textarea-left" >
-        <
-        div className = "textarea test-paragraph" > {
-            testInfo.map((individualletterInfo, index) => ( <
-                TestLetter key = { index }
-                individualletterInfo = { individualletterInfo }
+        <div className = "text-area-container">
+        <div className ="textarea-left">
+        <div className = "textarea test-paragraph"> 
+            {
+           testInfo.map((individualletterInfo, index) => ( 
+               <TestLetter 
+                    key = {index}
+                individualletterInfo = {individualletterInfo}
                 />
-            ))
-        } <
-        /div>  < /
-        div > <
-        div className = "textarea-right" >
-        <
-        textarea onChange = {
-            (e) =>
-            oninputchange(e.target.value)
-        }
+            ))}
+            
+            </div>  
+        </div>
+        <div className = "textarea-right" >
+        <textarea 
+                onChange = {(e) => oninputchange(e.target.value)}
         className = "textarea"
-        placeholder = "Start Typing" >
-        <
-        /textarea> < /
-        div > <
-        /div> < /
-        div >
+        placeholder = "Start Typing">
+        </textarea> 
+        </div> 
+        </div> 
+        </div>
     );
 };
 
