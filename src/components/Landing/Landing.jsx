b2e13dc4702f7b031282e4aba5c5f0434cbd1c49
Tarun <tarundyundi2000@gmail.com>
2021-06-21 19:54:26 +0530

    Update Landing.jsx

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -4,42 +4,38 @@ import flash from './../../assets/hero.png';
 import Typewriter from 'typewriter-effect';
 
 const Landing = () => {
-    return ( <
-        div className = "Landing-container" >
-        <
-        div //data - aos: "fade-right"
-        className = "Landing-left" >
-        <
-        h1 className = "Landing-header" > Can you type... < /h1> <
-        div className = "typewriter-container" >
+    return ( 
+        <div className = "Landing-container">
+        <div data-aos="fade-right"
+        className="Landing-left">
+        
+        <h1 className = "Landing-header" > Can you type... < /h1> 
+        <div className = "typewriter-container" >
 
-        <
-        Typewriter options = {
-            {
+        <Typewriter 
+        options = {{
                 strings: ['Fast?', 'Correct', 'Quick'],
                 autoStart: true,
                 loop: true,
-            }
-        }
-        /> < /
-        div > <
-        /div>
+            }}
+            
+        /> 
+        </div> 
+        </div>
 
-        <
-        div className = "Landing-right" >
-        <
-        img //data - aos = "fade-left"
-        className = "flash-image"
-        src = { flash }
-        alt = "hero" /
-        >
+        <div className="Landing-right">
+        <img 
+        data-aos="fade-left"
+        className="flash-image"
+        src={flash}
+        alt="hero" 
+        />
 
-        <
-        /div> < /
-        div >
+        </div> 
+        </div>
 
     );
 
-}
+};
 
 export default Landing;
